fdf36fc06455160fb096b969f64467b670f3acf3
aluno <etecae023@hotmail.com>
2025-04-07 22:09:48 -0300

    segunda avalicao

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RangeCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,42 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  valorRange = 0;
+  valorEmprestimo = 0;
+  taxaJuros = 0;
+  jurosDecimal = 0;
+  prazo = 0;
+  prestacao = 0;
+  totalEmprestimo = 0;
+  jurosAnual = 0;
+  anos =0;
+  mensagemEx1 = "";
+  onIonChange(ev:RangeCustomEvent) {
+    this.valorRange = parseInt(ev.detail.value.toString());
+  }
+  
+  calcularEx1(){
+
+
+    if(this.prazo > 11){
+      
+    this.jurosDecimal = this.taxaJuros/100;
+
+    this.anos = Math.ceil(this.prazo/23);
+
+    this.jurosAnual = this.valorEmprestimo*this.jurosDecimal*this.anos;
+
+    this.totalEmprestimo = this.valorEmprestimo + this.jurosAnual; 
+
+    this.prestacao = this.totalEmprestimo / Math.ceil(this.prazo);
+    
+      
+    } else {
+      this.totalEmprestimo = this.valorEmprestimo;
+      this.prestacao = this.totalEmprestimo/this.prazo;
+    }
+    
+  }
+
 
 }
